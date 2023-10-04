da55e2e9c8bdbf75e2b4ecf811f691170d13a228
Vishal Kumar Kushwaha <123886831+Kushwaha2219@users.noreply.github.com>
2023-10-04 20:44:14 +0530

    Roman Number to Integer.java

--- Roman Number to Integer.java
+++ Roman Number to Integer.java
@@ -3,7 +3,7 @@ class Solution {
         int n=str.length();
         HashMap<Character,Integer>hm=new HashMap<>();
         hm.put('I',1);hm.put('V', 5);hm.put('X', 10);hm.put('L', 50);hm.put('C', 100);hm.put('D', 500);
-        hm.put('M', 100);
+        hm.put('M', 1000);
         
         int res=hm.get(str.charAt(n-1));
         
